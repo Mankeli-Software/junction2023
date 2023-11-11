c7ee42e12ea1f2bba90d11d63a7b91735425d331
AkJkSenpai <aku.paananen100@gmail.com>
2023-11-11 16:27:17 +0200

    Add features

--- website/components/features.tsx
+++ website/components/features.tsx
@@ -21,8 +21,8 @@ export default function Features() {
                 <path className="stroke-current text-purple-100" d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd" />
                 <path className="stroke-current text-purple-300" d="M43 42h-9M43 37h-9" strokeLinecap="square" strokeWidth="2" />
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">Get rewards</h4>
+              <p className="text-lg text-gray-400 text-center">A lot of games reward spending countless hours playing. We can solve this by rewarding progression through physical activity.</p>
             </div>
 
             {/* 2nd item */}
@@ -32,8 +32,8 @@ export default function Features() {
                 <path className="stroke-current text-purple-100" strokeWidth="2" strokeLinecap="square" d="M21 23h22v18H21z" fill="none" fillRule="evenodd" />
                 <path className="stroke-current text-purple-300" d="M26 28h12M26 32h12M26 36h5" strokeWidth="2" strokeLinecap="square" />
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">Have fun with friends</h4>
+              <p className="text-lg text-gray-400 text-center">Compete with friends, play classic outdoor games and socialize. Enjoy your time together also offline!</p>
             </div>
 
             {/* 3rd item */}
@@ -46,8 +46,8 @@ export default function Features() {
                   <circle className="stroke-current text-purple-100" cx="11" cy="11" r="11" />
                 </g>
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">Haptic feedback</h4>
+              <p className="text-lg text-gray-400 text-center">Sensors found in everyday devices are used to create challenges to make outdoor games more engaging.</p>
             </div>
 
             {/* 4th item */}
@@ -59,8 +59,8 @@ export default function Features() {
                   <circle className="stroke-current text-purple-300" cx="13" cy="9" r="3" />
                 </g>
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">Real-time minigames</h4>
+              <p className="text-lg text-gray-400 text-center">Everybody loves power ups. Winning or losing minigames affect the state of the game by rewarding or punishing the players.</p>
             </div>
 
             {/* 5th item */}
@@ -72,8 +72,8 @@ export default function Features() {
                   <path className="stroke-current text-purple-300" d="M22 30h4v12h-4z" />
                 </g>
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">Customize the games</h4>
+              <p className="text-lg text-gray-400 text-center">Choose from a wide variety of minigames to enhance classic outdoor games. Hide and seek has never been this fun!</p>
             </div>
 
             {/* 6th item */}
@@ -86,8 +86,8 @@ export default function Features() {
                   <path className="stroke-current text-purple-100" d="M8.86 1.18C3.8 1.988 0 5.6 0 10c0 5 4.9 9 11 9a10.55 10.55 0 003.1-.4L20 21l-.6-5.2a9.125 9.125 0 001.991-2.948" />
                 </g>
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">Integrate to your systems</h4>
+              <p className="text-lg text-gray-400 text-center">Use our easy to use WebGUI and API to integrate J-mp to platforms your youngsters are already using. </p>
             </div>
 
           </div>
