d84487328de112214ff5f7f5a2875c0d7d5e4923
JareCoder <jesse.m.lappalainen@gmail.com>
2023-11-11 11:33:48 +0200

    ZigZag almost done heh :D

--- website/components/zigzag.tsx
+++ website/components/zigzag.tsx
@@ -30,25 +30,25 @@ export default function Zigzag() {
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
                 <div className="md:pr-4 lg:pr-12 xl:pr-16">
                   <h3 className="h3 mb-3">Rewarding Outdoor Play</h3>
-                  <p className="text-xl text-gray-400 mb-4">Combine existing digital hobbies with outdoor activities to create engaging, modern play experiences for youth. Take a break from the screen, but keep on progressing!</p>
+                  <p className="text-xl text-gray-400 mb-4">Combine existing digital hobbies with outdoor activities to create engaging, modern play experiences for youth. Now you can gain the same XP by going outside!</p>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Motivating kids to play outside by incorporating their existing digital hobbies.</span>
+                      <span>Incorporate existing digital hobbies.</span>
                     </li>
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Utilizing wearable tech to track and reward physical activity during outdoor games.</span>
+                      <span>Playing outside levels you up in your favourite game.</span>
                     </li>
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Digitalizing classic games like hide and seek with app-based challenges and interactive elements.</span>
+                      <span>Have fun with friends online and offline.</span>
                     </li>
                   </ul>
                 </div>
@@ -65,13 +65,13 @@ export default function Zigzag() {
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
                 <div className="md:pl-4 lg:pl-12 xl:pl-16">
                   <h3 className="h3 mb-3">Next-Gen Active Gaming</h3>
-                  <p className="text-xl text-gray-400 mb-4">Revitalize traditional games with modern tech! Sensors bring a new dimension to Hide'n'Seek, Tag, and more. Earn real rewards for moving around, and track your outdoor adventures with smart devices.</p>
+                  <p className="text-xl text-gray-400 mb-4">Revitalize traditional games with modern tech! Sensors bring a new dimension to Hide'n'Seek, Tag, and more. Earn real rewards from moving around, and track your outdoor adventures with smart devices.</p>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Innovate classic games like Hide’n’Seek and Tag with sensor technology for a thrilling experience.</span>
+                      <span>Utilizing wearable tech to track and reward physical activity.</span>
                     </li>
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
@@ -83,7 +83,7 @@ export default function Zigzag() {
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Amet consectetur adipiscing elit</span>
+                      <span>Digitalizing classic games with app-based challenges and interactive elements.</span>
                     </li>
                   </ul>
                 </div>
@@ -99,9 +99,8 @@ export default function Zigzag() {
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
                 <div className="md:pr-4 lg:pr-12 xl:pr-16">
-                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">More speed. Less spend</div>
-                  <h3 className="h3 mb-3">Keep projects on schedule</h3>
-                  <p className="text-xl text-gray-400 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <h3 className="h3 mb-3">Integrate to your system</h3>
+                  <p className="text-xl text-gray-400 mb-4">Create exciting challenges and offer rewards for outdoor activities. Integrate them directly to your PE class, reward program, digital hobby or anything else!</p>
                   <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
