0c43f325ad27a7709aec4fde3a4b4ba51e97eef8
Miska Kytö <45193204+Hylje5000@users.noreply.github.com>
2023-11-12 00:44:40 +0200

    added cool gif mockup

--- website/components/zigzag.tsx
+++ website/components/zigzag.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 import FeatImage01 from '@/public/images/paskamockup1.jpg'
-import FeatImage02 from '@/public/images/features-03-image-02.png'
+import FeatImage02 from '@/public/images/mockup2.gif'
 import FeatImage03 from '@/public/images/features-03-image-03.png'
 
 export default function Zigzag() {
