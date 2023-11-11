d3e25b38fc1a0304397f997b0c9c2a153b93313a
JareCoder <jesse.m.lappalainen@gmail.com>
2023-11-11 23:46:56 +0200

    WebGUI Header done

--- website/components/webpanel_header.tsx
+++ website/components/webpanel_header.tsx
@@ -1,9 +1,37 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import JmpLogoRound from '@/public/images/j-mp_icon_noBG.png'
+
 export default function WebpanelHeader() {
     return(
-    <header className="absolute w-full z-30">
-        <div className="bg-purple-700 text-center">
-            <h1 className="h1">J-mp WebGUI</h1>
+
+    <header className="absolute w-full z-30 ">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 bg-purple-700">
+        <div className="flex items-center h-full">
+          {/* Site branding */}
+          <div className="shrink-0 mr-4">
+            {/* Logo */}
+            <Link href="/" className="block" aria-label="Cruip">
+              <Image className="rounded-full" src={JmpLogoRound} width={48} height={48} alt="J-mp logo" />
+            </Link>
+          </div>
+          <div className="shrink-0 mr-4">
+            <h1 className="h1 center font-bold">J-mp WebGUI</h1>
+          </div>
+
+          {/* Desktop navigation */}
+          <nav className="hidden md:flex md:grow">
+            {/* Desktop sign in links */}
+            <ul className="flex grow justify-end flex-wrap items-center">
+              <li>
+                <Link href="/" className="btn-sm text-white bg-purple-600 hover:bg-purple-700 ml-3">
+                  Back
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
+      </div>
     </header>
     )
 }
