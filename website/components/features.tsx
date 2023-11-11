f4c83d82f32ff4d8870ae6bcd3279a680ea429a3
Miska Kytö <45193204+Hylje5000@users.noreply.github.com>
2023-11-11 13:50:32 +0200

    moved header from features to zigzag

--- website/components/features.tsx
+++ website/components/features.tsx
@@ -4,11 +4,7 @@ export default function Features() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20">
 
-          {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">Digital games are played by over 80% of Finns.</h2>
-            <p className="text-xl text-gray-400"><a className="underline" href="https://trepo.tuni.fi/handle/10024/144376">The Finnish Player Barometer</a> highlights Finnish gaming habits and to condense, we like to game.</p>
-          </div>
+          
 
           {/* Items */}
           <div className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
