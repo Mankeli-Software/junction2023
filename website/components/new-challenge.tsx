827848f079b6d3c556ab7d34aaee29120f6c1d9a
AkJkSenpai <aku.paananen100@gmail.com>
2023-11-12 02:53:24 +0200

    Customize new challenge dialog

--- website/components/new-challenge.tsx
+++ website/components/new-challenge.tsx
@@ -6,11 +6,63 @@ import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import Box from '@mui/material/Box';
+import { Theme, useTheme } from '@mui/material/styles';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import FormControl from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  'Haptic',
+  'Gyroscope',
+  'Accelometer',
+  'Location',
+  'Microphone',
+  'Compass',
+];
+
+function getStyles(name: string, personName: string[], theme: Theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
 
 export default function NewChallenge() {
+  const theme = useTheme();
   const [open, setOpen] = React.useState(false);
+  const [personName, setPersonName] = React.useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -20,6 +72,8 @@ export default function NewChallenge() {
     setOpen(false);
   };
 
+  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
   return (
     <React.Fragment>
       <Button className="btn-sm text-white ml-1 bg-purple-600 hover:bg-purple-500" variant="outlined" onClick={handleClickOpen}>
@@ -28,28 +82,101 @@ export default function NewChallenge() {
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Create new challenge</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We
-            will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Challenge name"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Challenge description"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
+          <Box>
+            <div>
+                <TextField
+                    id="title"
+                    label="Title"
+                    style={{ margin: 8 }}
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="standard"
+                />
+                <TextField
+                    id="subtitle"
+                    label="Subtitle"
+                    style={{ margin: 8 }}
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="standard"
+                />
+                <TextField
+                    id="description"
+                    label="Description"
+                    style={{ margin: 8 }}
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="standard"
+                />
+                <TextField
+                    id="imageurl"
+                    label="Image url"
+                    style={{ margin: 8 }}
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="standard"
+                />
+                <FormControl sx={{ m: 1, width: 300 }}>
+                    <InputLabel id="demo-multiple-name-label">Sensor types</InputLabel>
+                    <Select
+                    labelId="demo-multiple-name-label"
+                    id="demo-multiple-name"
+                    multiple
+                    value={personName}
+                    onChange={handleChange}
+                    input={<OutlinedInput label="Name" />}
+                    MenuProps={MenuProps}
+                    >
+                    {names.map((name) => (
+                        <MenuItem
+                        key={name}
+                        value={name}
+                        style={getStyles(name, personName, theme)}
+                        >
+                        {name}
+                        </MenuItem>
+                    ))}
+                    </Select>
+                </FormControl>
+                <TextField
+                    id="duration"
+                    label="Duration (min)"
+                    style={{ margin: 8 }}
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="standard"
+                    type="number"
+                />
+                <FormControl sx={{marginLeft: 1.25}}>
+                    <FormLabel sx={{fontSize: 12}} id="demo-row-radio-buttons-group-label">Difficulty</FormLabel>
+                    <RadioGroup
+                        row
+                        name="row-radio-buttons-group"
+                    >
+                        <FormControlLabel value="1" control={<Radio />} label="1" />
+                        <FormControlLabel value="2" control={<Radio />} label="2" />
+                        <FormControlLabel value="3" control={<Radio />} label="3" />
+                        <FormControlLabel value="4" control={<Radio />} label="4" />
+                        <FormControlLabel value="5" control={<Radio />} label="5" />
+                    </RadioGroup>
+                </FormControl>
+            </div>
+          </Box>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Create</Button>
