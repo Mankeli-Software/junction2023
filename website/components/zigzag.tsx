7e134a1504233567261b7cac3839bd23a76a035e
JareCoder <jesse.m.lappalainen@gmail.com>
2023-11-12 03:01:26 +0200

    LAst picture AAAAAAAAAAAAAAAAAAAA

--- website/components/zigzag.tsx
+++ website/components/zigzag.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 import FeatImage01 from '@/public/images/paskamockup1.jpg'
 import FeatImage02 from '@/public/images/mockup2.gif'
-import FeatImage03 from '@/public/images/features-03-image-03.png'
+import FeatImage03 from '@/public/images/control_panel.png'
 
 export default function Zigzag() {
   return (
